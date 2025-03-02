97034edc51f411f2cd1f79f99f1d7b39e5491ca4
Qingyu Wang <wangqingyu.c0l1n@bytedance.com>
2025-03-02 20:40:10 +0800

    fix(create-rspeedy): add `fullscreen=true` to schema (#2)
    This would provide better user experience when running on LynxExplorer.

--- examples/react/lynx.config.js
+++ examples/react/lynx.config.js
@@ -3,5 +3,13 @@ import { pluginQRCode } from '@lynx-js/qrcode-rsbuild-plugin';
 import { pluginReactLynx } from '@lynx-js/react-rsbuild-plugin';
 
 export default defineConfig({
-  plugins: [pluginReactLynx(), pluginQRCode()],
+  plugins: [
+    pluginReactLynx(),
+    pluginQRCode({
+      schema(url) {
+        // We use `?fullscreen=true` to open the page in LynxExplorer in full screen mode
+        return `${url}?fullscreen=true`;
+      },
+    }),
+  ],
 });
--- packages/rspeedy/create-rspeedy/template-react-ts/lynx.config.ts
+++ packages/rspeedy/create-rspeedy/template-react-ts/lynx.config.ts
@@ -5,7 +5,12 @@ import { pluginReactLynx } from '@lynx-js/react-rsbuild-plugin'
 
 export default defineConfig({
   plugins: [
-    pluginQRCode(),
+    pluginQRCode({
+      schema(url) {
+        // We use `?fullscreen=true` to open the page in LynxExplorer in full screen mode
+        return `${url}?fullscreen=true`
+      },
+    }),
     pluginReactLynx(),
   ],
 })
