11ef94a6a3a404d385d076f1bccc651aa0f21e7f
jfjfjfjfffff <xujinfengnb@gmail.com>
2025-03-06 21:55:32 +0800

    fix: Update constants.ts (#93)
    add as const
    
    Signed-off-by: jfjfjfjfffff <xujinfengnb@gmail.com>

--- packages/web-platform/web-constants/src/constants.ts
+++ packages/web-platform/web-constants/src/constants.ts
@@ -24,4 +24,4 @@ export const lynxViewRootDomId = 'lynx-view-root' as const;
 
 export const __lynx_timing_flag = '__lynx_timing_flag' as const;
 
-export const lynxViewEntryIdPrefix = 'lynx-view-id';
+export const lynxViewEntryIdPrefix = 'lynx-view-id' as const;
